e8a17a3bfd1b98a67b9b58e668511fd11100302d
Nagamadhu <nagamadhu025@gmail.com>
2026-02-08 09:27:31 +0530

    modified routes

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -29,8 +29,6 @@ function App() {
         <Route path="/fitness" element={<Fitness />} />
         <Route path="/todo" element={<Todo />} />
         <Route path="/profile" element={<Profile />} />
-
-
       </Routes>
     </Container>
   );
